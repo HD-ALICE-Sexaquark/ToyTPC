c78331362d882ebb0f7b139d4daffa8c85af3951
Andrés Bórquez <andres.borquezc@gmail.com>
2023-12-07 20:06:05 +0100

    injector: decided on n_times, and pt and y ranges

--- injector/GenBox.C
+++ injector/GenBox.C
@@ -11,66 +11,85 @@
 
 /*
  Generate some low-pT charged particles
-*/
+ */
 void GenBox(TString fOutputFilename = "../reconstruction/mc.csv") {
 
-    TDatabasePDG pdg;
+    // parameters
+    const Int_t n_times = 50;  // inject `n_times` particles per species
+    std::vector<Int_t> pdgVector = {211, 321, 2212};
+    std::vector<Float_t> fPtMin = {0.05, 0.1, 0.15};   // (in GeV/c)
+    std::vector<Float_t> fPtMax = {0.15, 0.25, 0.35};  // (in GeV/c)
+    std::vector<Float_t> fYMin = {-1.25, -0.85, -0.7};
+    std::vector<Float_t> fYMax = {1.25, 0.85, 0.7};
+    Float_t fPhiMin = 0.;               // (in radians)
+    Float_t fPhiMax = 2 * TMath::Pi();  // (in radians)
 
     // define output file
     std::ofstream fOutputFile;
     fOutputFile.open(fOutputFilename);
 
-    // params
-    std::vector<Int_t> pdgVector = {211, 321, 2212};
-    std::vector<Float_t> fPtMin = {0., 0., 0.};  // (in GeV/c)
-    std::vector<Float_t> fPtMax = {0.05, 0.1, 0.15};
-    Float_t fThetaMin = 0.439;                    // (in rad) ~ 25.15 rad ~ eta = 1.5
-    Float_t fThetaMax = TMath::Pi() - fThetaMin;  // (in rad) ~ 154.84 deg ~ eta = -1.5
-    Float_t fPhiMin = 0. * TMath::DegToRad();
-    Float_t fPhiMax = 360. * TMath::DegToRad();
-
+    // (debug)
     printf(">> Settings:\n");
-    printf("   (for pions)   %.2f < Pt < %.2f GeV/c\n", fPtMin[0], fPtMax[0]);
-    printf("   (for kaons)   %.2f < Pt < %.2f GeV/c\n", fPtMin[1], fPtMax[1]);
-    printf("   (for protons) %.2f < Pt < %.2f GeV/c\n", fPtMin[2], fPtMax[2]);
-    printf("   %.2f < Theta < %.2f degrees <=> %.2f < Theta < %.2f rad\n",  //
-           fThetaMin * TMath::RadToDeg(), fThetaMax * TMath::RadToDeg(), fThetaMin, fThetaMax);
-    printf("   %.2f < Phi < %.2f degrees <=> %.2f < Phi < %.2f rad\n\n",  //
-           fPhiMin * TMath::RadToDeg(), fPhiMax * TMath::RadToDeg(), fPhiMin, fPhiMax);
+    printf("   (for pions)   -> %.2f < Pt < %.2f GeV/c\n", fPtMin[0], fPtMax[0]);
+    printf("                 -> %.2f < Rapidity < %.2f\n", fYMin[0], fYMax[0]);
+    printf("   (for kaons)   -> %.2f < Pt < %.2f GeV/c\n", fPtMin[1], fPtMax[1]);
+    printf("                 -> %.2f < Rapidity < %.2f\n", fYMin[1], fYMax[1]);
+    printf("   (for protons) -> %.2f < Pt < %.2f GeV/c\n", fPtMin[2], fPtMax[2]);
+    printf("                 -> %.2f < Rapidity < %.2f\n", fYMin[2], fYMax[2]);
+    printf("   %.2f < Phi < %.2f rad\n\n", fPhiMin, fPhiMax);
+
+    // init PDG database
+    TDatabasePDG pdg;
 
     // prepare random numbers
     gRandom->SetSeed(0);
     Float_t fRandom[3];
 
+    // declare variables
+    Int_t current_particle_pdg;
+    Float_t Rapidity, Phi;
+    Float_t M, Mt, E;
+    Float_t Pt, Px, Py, Pl;
+    TLorentzVector Particle;
+    TString auxStr;
+
     for (Int_t sign : {1, -1}) {
 
         for (Int_t pp = 0; pp < (Int_t)pdgVector.size(); pp++) {
 
-            Int_t current_particle_pdg = sign * pdgVector[pp];
+            for (Int_t i = 0; i < n_times; i++) {
 
-            gRandom->RndmArray(3, fRandom);
+                current_particle_pdg = sign * pdgVector[pp];
 
-            // set particle properties
-            Float_t Pt = fPtMin[pp] + fRandom[0] * (fPtMax[pp] - fPtMin[pp]);  // pt (uniform distribution) (in GeV/c)
-            Float_t Theta = fThetaMin + fRandom[1] * (fThetaMax - fThetaMin);  // polar angle (uniform distribution) (in radians)
-            Float_t Eta = -TMath::Log(TMath::Tan(Theta / 2));                  // eta
-            Float_t Phi = fPhiMin + fRandom[2] * (fPhiMax - fPhiMin);          // azimuthal angle (uniform distribution) (in radians)
-            Float_t M = pdg.GetParticle(current_particle_pdg)->Mass();             // mass (in GeV/c^2)
+                gRandom->RndmArray(3, fRandom);
 
-            TLorentzVector Particle;
-            Particle.SetPtEtaPhiM(Pt, Eta, Phi, M);
+                // set particle properties
+                Pt = fPtMin[pp] + fRandom[0] * (fPtMax[pp] - fPtMin[pp]);     // Pt (uniform distribution) (in GeV/c)
+                Rapidity = fYMin[pp] + fRandom[1] * (fYMax[pp] - fYMin[pp]);  // rapidity (uniform distribution)
+                Phi = fPhiMin + fRandom[2] * (fPhiMax - fPhiMin);             // azimuthal angle (uniform distribution) (in radians)
+                M = pdg.GetParticle(current_particle_pdg)->Mass();            // mass (in GeV/c^2)
+                Px = Pt * TMath::Cos(Phi);                                    // Px (in GeV/c)
+                Py = Pt * TMath::Sin(Phi);                                    // Py (in GeV/c)
+                Mt = TMath::Sqrt(M * M + Pt * Pt);  // transverse msass (derived from inv. mass and Pt) (in GeV/c^2)
+                Pl = Mt * TMath::SinH(Rapidity);    // longitudinal momentum = Pz (derived from trans. mass and Y) (in GeV/c)
+                E = Mt * TMath::CosH(Rapidity);     // energy (derived from trans. mass and Y) (in GeV)
 
-            // print particle onto CSV file
-            TString auxStr = Form("%i,%.8e,%.8e,%.8e\n", current_particle_pdg, Particle.Px(), Particle.Py(), Particle.Pz());
+                Particle.SetPxPyPzE(Px, Py, Pl, E);
 
-            // (debug)
-            printf("%s", auxStr.Data());
+                // print particle onto CSV file
+                auxStr = Form("%i,%.8e,%.8e,%.8e\n", current_particle_pdg, Particle.Px(), Particle.Py(), Particle.Pz());
 
-            // (output)
-            fOutputFile << auxStr;
+                // (debug)
+                printf("%s", auxStr.Data());
+
+                // (output)
+                fOutputFile << auxStr;
+            }
         }
     }
 
     fOutputFile.close();
+
+    // (debug)
     printf("\n>> %s has been generated\n", fOutputFilename.Data());
 }
