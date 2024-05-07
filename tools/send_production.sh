5e65ff8abbc98372bf3dad24e2250f76520fad8b
A. Bórquez <andres.borquezc@gmail.com>
2024-05-07 23:32:24 +0200

    tools: updated prod. script to run on parallel tmux sessions

--- tools/send_production.sh
+++ tools/send_production.sh
@@ -24,25 +24,34 @@ function print_help() {
     echo "  * ROOT"
     echo "  * GEANT4"
     echo "USAGE:"
-    echo "  ./send_production.sh --run1 <run1> --run2 <run2> --serv <serv> --outsd <>"
-    echo "  ./send_production.sh --rn <rn> --serv <serv> --outsd <out_sub_dir>"
+    echo "  ./send_production.sh --mode <mode> --run1 <run1> --run2 <run2> --serv <serv> --job <job_dir>"
+    echo "  ./send_production.sh --mode <mode> --rn <rn> --serv <serv> --job <job_dir>"
     echo "  where:"
-    echo "  <rn>          = specific run numbers, separated by comma"
-    echo "                  for example: --rn 0,1,2"
-    echo "  <run1>        = run number of the first job (starting point of loop)"
-    echo "  <run2>        = run number of the last job (end of loop)"
-    echo "  <serv>        = select which machine to use: alice-serv<serv>"
-    echo "                  it can be: 10, 12 or 14"
-    echo "  <out_sub_dir> = subdirectory (it's a string!) within the output directory, for organizational purposes"
+    echo "  <mode>    = it can be:"
+    echo "              * 0 : send job to the htcondor farm"
+    echo "              * 1 : interactive execution in a tmux session (use with caution!)"
+    echo "  <job_dir> = subdirectory (it's a string!) within the output directory, for organizational purposes"
+    echo "  <rn>      = specific run numbers, separated by comma"
+    echo "              for example: --rn 0,1,2"
+    echo "  <run1>    = run number of the first job (starting point of loop)"
+    echo "  <run2>    = run number of the last job (end of loop)"
+    echo "  <serv>    = (necessary and only valid when mode is 0)"
+    echo "              select which machine to use: alice-serv<serv>"
+    echo "              it can be: 10, 12 or 14"
     echo "EXAMPLE:"
-    echo "  ./send_production.sh --run1 1 --run2 100 --serv 10 --outsd 001"
+    echo "  ./send_production.sh --mode 0 --job 01 --run1 0 --run2 99 --serv 10"
+    echo "  ./send_production.sh --mode 1 --job 00 --run1 0 --run2 10"
 }
 
 function process_args() {
     arr=("$@")
     ic=0
     while [[ $ic -le $((${#arr[@]}-1)) ]]; do
-        if [[ "${arr[$ic]}" == "--rn" ]]; then
+        if [[ "${arr[$ic]}" == "--mode" ]]; then
+            INT_MODE=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--job" ]]; then
+            JOB_DIR=${arr[$((ic+1))]}
+        elif [[ "${arr[$ic]}" == "--rn" ]]; then
             RUN_NUMBER_LIST=${arr[$((ic+1))]}
             RUN_NUMBER_ARR=(${RUN_NUMBER_LIST//,/ }) # convert comma-separated list into array of numbers
         elif [[ "${arr[$ic]}" == "--run1" ]]; then
@@ -51,10 +60,6 @@ function process_args() {
             RUN2=${arr[$((ic+1))]}
         elif [[ "${arr[$ic]}" == "--serv" ]]; then
             SERV=${arr[$((ic+1))]}
-        elif [[ "${arr[$ic]}" == "--sim_dir" ]]; then
-            SIM_DIR=${arr[$((ic+1))]}
-        elif [[ "${arr[$ic]}" == "--outsd" ]]; then
-            OUTPUT_SUBDIR=${arr[$((ic+1))]}
         elif [[ "${arr[$ic]}" == "--help" ]]; then
             print_help
             exit 0
@@ -64,6 +69,58 @@ function process_args() {
         fi
         ((ic+=2))
     done
+}
+
+  ##
+ ## TMUX management
+# # # # # # # # # #
+
+function prepare_and_exec_tmux() {
+
+    # create execution file
+    run_file="${STR_RUN}.sh"
+
+    # (note to self: duplicated code, it's the same as in prepare_execution_script...)
+    echo "#!/bin/bash"                                                         > ${run_file}
+    echo ""                                                                   >> ${run_file}
+    ### run injector
+    echo "for ((event=0; event < ${N_EVENTS_PER_RUN}; event++)); do"                >> ${run_file}
+    echo '    event_id=$(printf "%03d" ${event})'                                   >> ${run_file}
+    echo '    MC_CSV=event${event_id}_mc.csv'                                       >> ${run_file}
+    echo '    MC_LOG=event${event_id}_mc.log'                                       >> ${run_file}
+    echo '    root -l -b -q '\''GenBox.C("'\''${MC_CSV}'\''")'\'' &> ${MC_LOG}'     >> ${run_file}
+    echo '    echo "'${run_file}' :: injecting event ${event_id}"'                  >> ${run_file}
+    echo "done"                                                                     >> ${run_file}
+    echo "cat event*_mc.log > ${STR_RUN}_mc.log"                                    >> ${run_file}
+    echo "rm event*_mc.log"                                                         >> ${run_file}
+    echo ""                                                                         >> ${run_file}
+    ### do reconstruction
+    echo "for ((event=0; event < ${N_EVENTS_PER_RUN}; event++)); do"                >> ${run_file}
+    echo '    event_id=$(printf "%03d" ${event})'                                   >> ${run_file}
+    echo '    MC_CSV=event${event_id}_mc.csv'                                       >> ${run_file}
+    echo '    TRAJ_CSV=event${event_id}_traj.csv'                                   >> ${run_file}
+    echo '    ITS_CSV=event${event_id}_its.csv'                                     >> ${run_file}
+    echo '    TPC_CSV=event${event_id}_tpc.csv'                                     >> ${run_file}
+    echo '    RECO_LOG=event${event_id}_reco.log'                                   >> ${run_file}
+    echo '    echo "'${run_file}' :: reconstructing event ${event_id}"'             >> ${run_file}
+    echo '    ./main ${MC_CSV} ${TRAJ_CSV} ${ITS_CSV} ${TPC_CSV} &> ${RECO_LOG}'    >> ${run_file}
+    echo 'done'                                                                     >> ${run_file}
+    echo "cat event*_reco.log > ${STR_RUN}_reco.log"                                >> ${run_file}
+    echo "rm event*_reco.log"                                                       >> ${run_file}
+    echo ""                                                                         >> ${run_file}
+    ### merge csv files
+    echo './merge_files.sh $(readlink -f .)'                                        >> ${run_file}
+
+    chmod a+x ${run_file}
+
+    # create tmux session
+    THIS_SESSION="${JOB_DIR}_${STR_RUN}"
+    tmux new -d -s ${THIS_SESSION}
+
+    # prepare environment
+    tmux send -t ${THIS_SESSION} "source set_env.sh" ENTER
+    tmux send -t ${THIS_SESSION} "./${run_file} 2> stderr.log 1> stdout.log" ENTER
+    tmux send -t ${THIS_SESSION} "exit" ENTER
 }
 
   ##
@@ -105,7 +162,7 @@ function prepare_execution_script() {
     echo "cat event*_reco.log > ${STR_RUN}_reco.log"                                >> ${run_file}
     echo "rm event*_reco.log"                                                       >> ${run_file}
     echo ""                                                                         >> ${run_file}
-    ### merge csv files (note to self: can improve...)
+    ### merge csv files (note to self: can be improved...)
     echo './merge_files.sh $(readlink -f .)'                                        >> ${run_file}
     echo 'tmp_dir=$(readlink -f .)'                                                 >> ${run_file}
     echo 'tmp_dir=${tmp_dir##*/}'                                                   >> ${run_file}
@@ -153,7 +210,12 @@ process_args "${argArray[@]}"
  ## Check for command-line errors
 # # # # # # # # # # # # # # # # # #
 
-if [[ -z ${SERV} ]]; then
+if [[ -z ${INT_MODE} ]]; then
+    echo "ERROR: --mode option empty, use --help to see available commands"
+    exit 1
+fi
+
+if [[ ${INT_MODE} -eq 0 ]] && [[ -z ${SERV} ]]; then
     echo "ERROR: --serv option empty, use --help to see available commands"
     exit 1
 fi
@@ -230,9 +292,11 @@ echo "send_production.sh :: >> N_EVENTS_PER_RUN = ${N_EVENTS_PER_RUN}"
 echo "send_production.sh ::"
 echo "send_production.sh :: chosen options:"
 echo "send_production.sh :: >> sim_dir  = ${SIM_DIR}"
+echo "send_production.sh :: >> mode     = ${INT_MODE}"
+echo "send_production.sh :: >> job_dir  = ${JOB_DIR}"
 echo -n "send_production.sh :: >> runs     = "
 for run in ${RUN_NUMBER_ARR[@]}; do
-    echo -n ''$(printf "%03d" ${run})' '
+    echo -n ''$(printf "%02d" ${run})' '
 done
 echo ""
 echo "send_production.sh :: "
@@ -254,10 +318,10 @@ fi
 for run in ${RUN_NUMBER_ARR[@]}; do
 
     # define run number
-    STR_RUN=run$(printf "%03d" ${run})
+    STR_RUN=run$(printf "%02d" ${run})
 
     # define the specific output dir for this run
-    RUN_DIR=${OUTPUT_DIR}/${OUTPUT_SUBDIR}/${STR_RUN}
+    RUN_DIR=${OUTPUT_DIR}/${JOB_DIR}/${STR_RUN}
     mkdir -p ${RUN_DIR}
 
     # enter output dir
@@ -269,8 +333,12 @@ for run in ${RUN_NUMBER_ARR[@]}; do
     cp ${SIM_DIR}/reconstruction/G4Setup_build/main .
     cp ${SIM_DIR}/tools/merge_files.sh .
 
-    prepare_execution_script
-    create_and_send_job
+    if [[ ${INT_MODE} -eq 1 ]]; then
+        prepare_and_exec_tmux
+    else
+        prepare_execution_script
+        create_and_send_job
+    fi
 
     # go back to sim dir
     cd ${SIM_DIR}
