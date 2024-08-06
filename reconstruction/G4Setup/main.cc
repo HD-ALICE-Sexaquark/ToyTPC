0d652dc07f47a3e5bab74e70e145afd350a790ff
A. Bórquez <andres.borquezc@gmail.com>
2024-08-06 23:59:21 +0200

    reco: number of threads and mag field as option

--- reconstruction/G4Setup/main.cc
+++ reconstruction/G4Setup/main.cc
@@ -37,6 +37,7 @@
 #include "G4StateManager.hh"
 #include "G4StepLimiterPhysics.hh"
 #include "G4SteppingVerbose.hh"
+#include "G4SystemOfUnits.hh"
 #include "G4UImanager.hh"
 
 #include "Randomize.hh"
@@ -63,22 +64,25 @@ int main(int argc, char** argv) {
     G4String output_filename_traj;
     G4String output_filename_ITS;
     G4String output_filename_TPC;
+    G4float magfield_z;
 
     if (argc == 1) {
         ui = new G4UIExecutive(argc, argv);
-    } else if (argc == 5) {
+    } else if (argc == 6) {
         input_filename = argv[1];
         output_filename_traj = argv[2];
         output_filename_ITS = argv[3];
         output_filename_TPC = argv[4];
+        magfield_z = std::stof(argv[5]) * tesla;
     } else {
+        // clang-format off
         G4cerr << "main.cc :: ERROR: incorrect number of arguments." << G4endl;
         G4cerr << "main.cc ::        -> for command-line mode, you need exactly four arguments:" << G4endl;
-        G4cerr << "main.cc ::           ./main <input_filename> <output_filename_traj> <output_filename_ITS> <output_filename_TPC>"
-               << G4endl;
+        G4cerr << "main.cc ::           ./main <input_filename> <output_filename_traj> <output_filename_ITS> <output_filename_TPC> <magfield_z in tesla>" << G4endl;
         G4cerr << "main.cc ::        -> for graphic-interactive mode, you need no arguments:" << G4endl;
         G4cerr << "main.cc ::           ./main" << G4endl;
         return 1;
+        // clang-format on
     }
 
     // (debug)
@@ -87,6 +91,7 @@ int main(int argc, char** argv) {
     G4cout << "main.cc :: >> output_filename_traj = " << output_filename_traj << G4endl;
     G4cout << "main.cc :: >> output_filename_ITS  = " << output_filename_ITS << G4endl;
     G4cout << "main.cc :: >> output_filename_TPC  = " << output_filename_TPC << G4endl;
+    G4cout << "main.cc :: >> magfield_z           = " << magfield_z / tesla << " tesla" << G4endl;
 
     // optional: choose a different random engine...
     // G4Random::setTheEngine(new CLHEP::MTwistEngine);
@@ -122,11 +127,13 @@ int main(int argc, char** argv) {
     const G4Run* run = nullptr;
     const std::vector<const G4Event*>* events = nullptr;
     G4int nKeptEvents;
-    G4int nAttempts = 1000;  // hardcoded value, so job doesn't run forever
+    G4String nProcesses = getenv("GEANT4_NUM_THREADS") ? getenv("GEANT4_NUM_THREADS") : "1";  // left as a string on purpose
+    G4int nAttempts = int(1000. / std::stof(nProcesses));                                     // hardcoded value, so job doesn't run forever
 
     // Process macro or start UI session
     if (!ui) {
         // batch mode
+        UImanager->ApplyCommand("/run/numberOfThreads " + nProcesses);
         UImanager->ApplyCommand("/run/initialize");  // G4RunManager::Initialize().
         UImanager->ApplyCommand("/tracking/verbose 0");
         UImanager->ApplyCommand("/tracking/storeTrajectory 2");  // IMPORTANT!!
@@ -134,9 +141,9 @@ int main(int argc, char** argv) {
         UImanager->ApplyCommand("/ALICE/traj_file " + output_filename_traj);
         UImanager->ApplyCommand("/ALICE/its_file " + output_filename_ITS);
         UImanager->ApplyCommand("/ALICE/tpc_file " + output_filename_TPC);
-        UImanager->ApplyCommand("/globalField/setValue 0 0 0.2 tesla");
+        UImanager->ApplyCommand("/globalField/setValue 0 0 " + std::to_string(magfield_z / tesla) + " tesla");
         for (G4int i = 0; i < nAttempts; i++) {
-            UImanager->ApplyCommand("/run/beamOn 1");
+            UImanager->ApplyCommand("/run/beamOn " + nProcesses);
             run = runManager ? runManager->GetCurrentRun() : nullptr;
             events = run ? run->GetEventVector() : nullptr;
             nKeptEvents = events ? (G4int)events->size() : 0;
--- reconstruction/run_cli.sh
+++ reconstruction/run_cli.sh
@@ -7,6 +7,6 @@ BUILD_DIR=$(readlink -f ./G4Setup_build)
 
 cd ${BUILD_DIR}
 
-./main ../mc.csv ../traj.csv ../its.csv ../tpc.csv
+./main ../mc.csv ../traj.csv ../its.csv ../tpc.csv 0.2 2>&1 | tee ../rec.log
 
 cd ${CURRENT_DIR}
